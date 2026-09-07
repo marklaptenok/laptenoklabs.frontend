82ffe2efcebb3d8bccdd8d4add2fb885f697f477
Mark Laptenok <mark@codelearning.online>
2026-09-07 15:21:05 +0200

    Updated Astro to 7.3.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,12 +21,10 @@ export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: "Cormorant",
-            cssVariable: "--font-main",
-            styles: ["normal"],
-        }]
-    }
+    fonts: [{
+        provider: fontProviders.google(),
+        name: "Cormorant",
+        cssVariable: "--font-main",
+        styles: ["normal"],
+    }],
 });
